23094b21efe91315f9a22f258147de6cfe548a9d
Filipe Cruz <filipe.cruz.ribeiro@gmail.com>
2022-10-20 22:19:39 -0300

    uploading files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MainQuiz from './mainquiz';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MainQuiz/>
+  </React.StrictMode>
+);
+
---
+++ src/quizData.js
@@ -0,0 +1,21 @@
+export const quizData = [
+    {
+      id: 0,
+      question: ` Qual é a capital do Brasil`,
+      options: [`Salvador`, `Rio de Janeiro`, `Brasília`],
+      answer: `Brasília`
+    },
+    {
+      id: 1,
+      question: `Quem descobriu a lei da gravidade`,
+      options: [`Isaac Newton`, `Albert Einstein`, `Elon Musk`],
+      answer: `Isaac Newton`
+    },
+    {
+      id: 3,
+      question: `Quem inventou o facebook`,
+      options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+      answer: `Mark Zuckerberg`
+    },
+  ];
+  
